49fec465e06ce2b723eb6920d473533efa624aa2
Mamun746 <abdullahalmamun914@gmail.com>
2020-01-05 11:05:43 +0600

    initial

---
+++ src/component/Car.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Car extends Component {
+    render() {
+        return (
+            <div>
+                dfwf
+            </div>
+        );
+    }
+}
+
+export default Car;
---
+++ src/component/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Search from '../container/Search'
+
+class Home extends Component {
+    render() {
+        return (
+            <div className="Home">
+               <Search/>
+            </div>
+        );
+    }
+}
+
+export default Home;
